344233e543a2d1f811dee4fa38ce2af4e9d0f3a5
Juan <Ju.pardob@alumnos.duoc.cl>
2020-09-23 19:45:53 -0300

    menu segun role

--- src/app/shared/model/url.model.ts
+++ src/app/shared/model/url.model.ts
@@ -1,6 +1,6 @@
 export class Urls {
 
-    private urlBase: string = "https://4941fb63fb26.ngrok.io/api"
+    private urlBase: string = "http://localhost:5004/api"//"https://4941fb63fb26.ngrok.io/api"
     public urlsToLogin = {
         signIn: this.urlBase + "/auth/signin",
         signUp: this.urlBase + "/auth/signup",
